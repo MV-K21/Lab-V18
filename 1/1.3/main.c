213ec1ddf13847884eca72c75bf93241f9958612
Tvar1233 <65509058+Tvar1233@users.noreply.github.com>
2020-06-29 13:07:36 +0300

    Update main.c

--- 1/1.3/main.c
+++ 1/1.3/main.c
@@ -2,13 +2,9 @@
 
 int main()
 {
-float x,y,r,h;
-printf("Координати точки: ");
-    scanf("%f%f", &x,&y);
-    printf("Радіус круга: ");
-    scanf("%f", &r);
-    h = (x*x + y*y);
-    printf("Гіпотенуза рівна %.2f. ", h);
-    if (h > r) printf("Точка не належить кругу.\n");
-    else printf("Точка належить кругу.\n");
+   int x,res;
+   printf("x=");
+   scanf("%d",&x);
+   res = x / 60;
+   printf("%d\n прошло з початку доби",res);
 }
